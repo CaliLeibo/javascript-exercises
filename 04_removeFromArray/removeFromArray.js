ff98c9dd935b7c0f3ccef6c976c8f9043d780a87
Cali Leibovitz <calileibo@yahoo.com>
2024-06-20 18:56:19 -0400

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    const totalArgs = [];
+    const newArray = [];
+    for (const args of theArgs){
+        totalArgs.push(args);
+    }
+
+    console.log(totalArgs);
+
+    outer: for (const arr of array){
+        for (const total of totalArgs){
+            if (total === arr){
+                continue outer;
+            }   
+        }        
+        newArray.push(arr);
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
