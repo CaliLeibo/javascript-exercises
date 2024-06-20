78b72a340fdd467fb4fef011e66bd19a292b98a8
Cali Leibovitz <calileibo@yahoo.com>
2024-06-20 17:59:49 -0400

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = "";
 
+    for (let i = 1; i < string.length+1; i++){
+        reverse += string.at(string.length-i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
