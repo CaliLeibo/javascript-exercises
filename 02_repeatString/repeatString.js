d7abf917a3d9e267ad7156b1180a3884e3df989e
Cali Leibovitz <calileibo@yahoo.com>
2024-06-20 17:33:13 -0400

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeats = "";
 
+    if (num < 0){
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < num; i++){
+        repeats += string;
+    }
+    return repeats;
 };
 
 // Do not edit below this line
