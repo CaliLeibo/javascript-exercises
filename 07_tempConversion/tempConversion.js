2f5243a15926c00ab3367a61d4f8b27f24e1b465
Cali Leibovitz <calileibo@yahoo.com>
2024-06-20 22:09:42 -0400

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let conversion = (5/9)*(num-32);
+  return parseFloat(conversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let conversion = (9/5)*num+32;
+  return parseFloat(conversion.toFixed(1));
 };
 
 // Do not edit below this line
