f759fa1edcd9af3c607a55cdee54af2ac0d91d49
Cali Leibovitz <calileibo@yahoo.com>
2024-06-20 21:28:07 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number'){
+        return 'ERROR';
+    }
 
+    let totalSum = 0;
+    if (b > a){
+        let i = 1;
+        totalSum += b;
+        while ((b-i) >= a){
+            totalSum += (b-i);
+            i++;
+        }
+    }
+    else {
+        let i = 1;
+        totalSum += a;
+        while ((a-i) >= b){
+            totalSum += (a-i);
+            i++;
+        }
+    }
+    console.log(totalSum);
+    return totalSum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
